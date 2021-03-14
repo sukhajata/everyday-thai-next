242c2bcc4dc9de165d847e53fc4f5dde26d817bb
timcranfield <tcranfield@electronet.co.nz>
2021-03-14 14:57:59 +1300

    Changed app to function component

--- pages/_app.js
+++ pages/_app.js
@@ -1,28 +1,12 @@
-import App from 'next/app';
 //import {Provider} from 'react-redux';
 import React from 'react';
 import { ScoreProvider } from '../context/ScoreContext';
 import '../styles/global.css'
 
-class MyApp extends App {
-/*
-    static async getInitialProps({Component, ctx}) {
-        const pageProps = Component.getInitialProps ? await Component.getInitialProps(ctx) : {};
+const Application = ({ Component, pageProps }) => (
+    <ScoreProvider>
+        <Component {...pageProps}/>
+    </ScoreProvider>
+);
 
-        //Anything returned here can be accessed by the client
-        return {pageProps: pageProps};
-    }
-*/
-    render() {
-        //pageProps that were returned  from 'getInitialProps' are stored in the props i.e. pageprops
-        const {Component, pageProps} = this.props;
-
-        return (
-            <ScoreProvider>
-                <Component {...pageProps}/>
-            </ScoreProvider>
-        );
-    }
-}
-
-export default MyApp;
+export default Application;
