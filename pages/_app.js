00db171eaa4949675726b78bfcd54bb4b0d95d0a
timcranfield <tcranfield@electronet.co.nz>
2021-03-14 14:53:17 +1300

    Removed getInitialProps

--- pages/_app.js
+++ pages/_app.js
@@ -5,14 +5,14 @@ import { ScoreProvider } from '../context/ScoreContext';
 import '../styles/global.css'
 
 class MyApp extends App {
-
+/*
     static async getInitialProps({Component, ctx}) {
         const pageProps = Component.getInitialProps ? await Component.getInitialProps(ctx) : {};
 
         //Anything returned here can be accessed by the client
         return {pageProps: pageProps};
     }
-
+*/
     render() {
         //pageProps that were returned  from 'getInitialProps' are stored in the props i.e. pageprops
         const {Component, pageProps} = this.props;
